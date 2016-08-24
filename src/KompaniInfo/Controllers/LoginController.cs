f2de9a1d2bc7631be2a5911e20ba20f40420d51a
Daniel <daniel.hermansson@gmail.com>
2016-08-24 20:26:19 +0200

    lagt till lösenord i config och provat publicera till http://hmvblginfo.azurewebsites.net

--- src/KompaniInfo/Controllers/LoginController.cs
+++ src/KompaniInfo/Controllers/LoginController.cs
@@ -7,54 +7,60 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Http.Authentication;
+using Microsoft.Extensions.Configuration;
 
 namespace KompaniInfo.Controllers
 {
-	[AllowAnonymous]
-	public class LoginController : Controller
+  [AllowAnonymous]
+  public class LoginController : Controller
+  {
+    private IConfiguration Configuration { get; set; }
+    public LoginController(IConfiguration configuration)
     {
-		public IActionResult Index()
-		{
-			return View();
-		}
+      Configuration = configuration;
+    }
+    public IActionResult Index()
+    {
+      return View();
+    }
 
-		[HttpPost]
-        public  IActionResult Index(string password)
+    [HttpPost]
+    public IActionResult Index(string password)
+    {
+      if (password == Configuration["UserPassword"])
+      {
+        Login(Roles.User);
+      }
+      else if (password == Configuration["AdminPassword"])
+      {
+        Login(Roles.Admin);
+      }
+      return View();
+    }
+
+    public IActionResult Logout()
+    {
+      HttpContext.Authentication.SignOutAsync("Cookie");
+      return View();
+    }
+
+    async private void Login(string role)
+    {
+      const string Issuer = "hmvblg";
+      var claims = new List<Claim>();
+      claims.Add(new Claim(ClaimTypes.Role, role, ClaimValueTypes.String, Issuer));
+      var userIdentity = new ClaimsIdentity("HmvBlgIdentity");
+      userIdentity.AddClaims(claims);
+      var userPrincipal = new ClaimsPrincipal(userIdentity);
+
+      await HttpContext.Authentication.SignInAsync("Cookie", userPrincipal,
+        new AuthenticationProperties
         {
-			if (password == "user")
-			{
-				Login(Roles.User);
-			}
-			else if(password == "admin")
-			{
-				Login(Roles.Admin);
-			}
-			return View();
-        }
+          ExpiresUtc = DateTime.UtcNow.AddMinutes(60),
+          IsPersistent = false,
+          AllowRefresh = false
+        });
+    }
 
-		public IActionResult Logout()
-		{
-			HttpContext.Authentication.SignOutAsync("Cookie");
-			return View();
-		}
-
-		async private void Login(string role)
-		{
-			const string Issuer = "hmvblg";
-			var claims = new List<Claim>();
-			claims.Add(new Claim(ClaimTypes.Role, role, ClaimValueTypes.String, Issuer));
-			var userIdentity = new ClaimsIdentity("HmvBlgIdentity");
-			userIdentity.AddClaims(claims);
-			var userPrincipal = new ClaimsPrincipal(userIdentity);
-
-			await HttpContext.Authentication.SignInAsync("Cookie", userPrincipal,
-				new AuthenticationProperties
-				{
-					ExpiresUtc = DateTime.UtcNow.AddMinutes(60),
-					IsPersistent = false,
-					AllowRefresh = false
-				});
-		}
-
-	}
+  }
 }
--- src/KompaniInfo/Startup.cs
+++ src/KompaniInfo/Startup.cs
@@ -13,66 +13,69 @@ using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace KompaniInfo
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IHostingEnvironment env)
     {
-        public Startup(IHostingEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
-        }
+      var builder = new ConfigurationBuilder()
+          .SetBasePath(env.ContentRootPath)
+          .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+          .AddEnvironmentVariables();
 
-        public IConfigurationRoot Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-			services.AddAuthorization();
-			services.AddMvc(config =>
-			{
-				var policy = new AuthorizationPolicyBuilder()
-								 .RequireAuthenticatedUser()
-								 .Build();
-				config.Filters.Add(new AuthorizeFilter(policy));
-			});
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-        {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseBrowserLink();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
-
-            app.UseStaticFiles();
-
-			app.UseCookieAuthentication(new CookieAuthenticationOptions
-			{
-				AuthenticationScheme = "Cookie",
-				LoginPath = new PathString("/Login/Index/"),
-				AccessDeniedPath = new PathString("/Login/Forbidden/"),
-				AutomaticAuthenticate = true,
-				AutomaticChallenge = true
-			});
-
-			app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
-        }
+      Configuration = builder.Build();
     }
+
+    public IConfigurationRoot Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddAuthorization();
+      services.AddMvc(config =>
+      {
+        var policy = new AuthorizationPolicyBuilder()
+                 .RequireAuthenticatedUser()
+                 .Build();
+        config.Filters.Add(new AuthorizeFilter(policy));
+      });
+
+      services.AddSingleton<IConfiguration>(Configuration);
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+    {
+      loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+      loggerFactory.AddDebug();
+
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+        app.UseBrowserLink();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Home/Error");
+      }
+
+      app.UseStaticFiles();
+
+      app.UseCookieAuthentication(new CookieAuthenticationOptions
+      {
+        AuthenticationScheme = "Cookie",
+        LoginPath = new PathString("/Login/Index/"),
+        AccessDeniedPath = new PathString("/Login/Forbidden/"),
+        AutomaticAuthenticate = true,
+        AutomaticChallenge = true
+      });
+
+      app.UseMvc(routes =>
+            {
+              routes.MapRoute(
+                  name: "default",
+                  template: "{controller=Home}/{action=Index}/{id?}");
+            });
+    }
+  }
 }
