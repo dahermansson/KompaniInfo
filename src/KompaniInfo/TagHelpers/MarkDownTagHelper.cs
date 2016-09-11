63d8c2f43884bac8c0e2ea6c1ee88ee9734eec74
Daniel <daniel.hermansson@gmail.com>
2016-09-11 13:35:28 +0200

    tog bort namespeces
    Fler namespaces

--- src/KompaniInfo/TagHelpers/MarkDownTagHelper.cs
+++ src/KompaniInfo/TagHelpers/MarkDownTagHelper.cs
@@ -1,10 +1,5 @@
 ﻿using HeyRed.MarkdownSharp;
-using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Razor.TagHelpers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace KompaniInfo.TagHelpers
 {
