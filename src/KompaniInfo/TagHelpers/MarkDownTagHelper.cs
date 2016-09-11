5207df7036faff4a75965f0aa66957f9a8164a31
Daniel <daniel.hermansson@gmail.com>
2016-09-11 13:27:05 +0200

    En tag helper för markdown istärrte för extensions Fix #10

---
+++ src/KompaniInfo/TagHelpers/MarkDownTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using HeyRed.MarkdownSharp;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KompaniInfo.TagHelpers
+{
+  [HtmlTargetElement("markdown", Attributes = MarkdownAttributeName)]
+  public class MarkDownTagHelper : TagHelper
+  {
+    private const string MarkdownAttributeName = "asp-markdown-for";
+
+    [HtmlAttributeName("asp-markdown-for")]
+    public string MarkDown { get; set; }
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+      output.Content.SetHtmlContent(new Markdown().Transform(MarkDown));
+    }
+  }
+}
